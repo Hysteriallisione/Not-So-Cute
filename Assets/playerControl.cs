e5d1ec16cccd59f52babffe2e24d339c40a3749f
Hysteriallisione <yangtchenheitz@yahoo.com.au>
2022-11-08 16:15:45 +0100

    scripting and anim
    add new game object of Bust with its anim, anim and blend tree not working yet but moving working. need to check on house collider to adjust. need to make script for camera

--- Assets/playerControl.cs
+++ Assets/playerControl.cs
@@ -10,20 +10,43 @@ public class playerControl : MonoBehaviour
     public Animator animaP;
     public float Horizontal;
     public float Vertical;
+    private bool moove;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animaP = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        moove = false;
         Vector2 movingP = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         playerVect = movingP.normalized * rapide;
+        Horizontal = transform.position.x;
+        Vertical = transform.position.y;
+       
+       
 
-        Horizontal = Input.GetAxisRaw("Horizontal");
-        Vertical = Input.GetAxisRaw("Vertical");
+        if (movingP.magnitude > 0)
+        {
+            moove=true;
+            animaP.SetFloat("Horizontal", movingP.x);
+            animaP.SetFloat("Vertical", movingP.y);
+            animaP.SetBool("playerMove", true);
+            animaP.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+            animaP.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+        }
+        else
+        {
+            moove = false;
+            animaP.SetBool("playerMove", false);
+        }
+    }
+    void FixedUpdate()
+    {
+
+        rigidB.MovePosition(rigidB.position + playerVect * Time.fixedDeltaTime * rapide);
     }
 }
