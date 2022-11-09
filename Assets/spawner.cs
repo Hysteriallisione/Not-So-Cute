ee68de446d8202355c70b236f898cb149bd0d22e
Hysteriallisione <yangtchenheitz@yahoo.com.au>
2022-11-09 15:43:17 +0100

    colliders, layers and spawner start
    rectification des colliders de la maison, colliders de limites de terrain jouable et début du script de spawner de game objects "cute"

---
+++ Assets/spawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public int numOfCute;
+    public List<GameObject> cuteObj;
+    public GameObject right;
+    public GameObject left;
+    public GameObject top;
+    public GameObject bottom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void spawnObjects()
+    {
+      //  int randomCute = 12;
+        //for(int i = 0; i < numOfCute; i++)
+        //{
+
+        //}
+    }
+}
