8fa3bfca3ca2d2e56715caa37b89f1c02376d09e
Hysteriallisione <yangtchenheitz@yahoo.com.au>
2022-11-10 09:58:42 +0100

    travail du spawn
    instantiate arrangé pour du random de "cute" game objects.

--- Assets/spawner.cs
+++ Assets/spawner.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class spawner : MonoBehaviour
 {
     public int numOfCute;
-    public List<GameObject> cuteObj;
-    public GameObject right;
-    public GameObject left;
-    public GameObject top;
-    public GameObject bottom;
+    public BoxCollider2D[] borders;
+    public float betweenspawns;
+    private float lastCute;
+    public AnimationCurve curveCute;
+    public float timePlaying;
+    public Transform cuteGroup;
+    public GameObject[] cuteBox;
+     
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +21,24 @@ public class spawner : MonoBehaviour
     }
 
     // Update is called once per frame
-    void spawnObjects()
+    void Update()
     {
-      //  int randomCute = 12;
-        //for(int i = 0; i < numOfCute; i++)
-        //{
+        float started = Time.timeSinceLevelLoad; 
+        betweenspawns = curveCute.Evaluate(started/100f);
 
-        //}
+
+         if(Time.time > lastCute + betweenspawns)
+         {
+       //on random le Vector2
+        int zone = Random.Range(0, borders.Length);
+        int randomCuBindex = Random.Range(0, cuteBox.Length);
+        Vector2 pos = new Vector2(
+            Random.Range(borders[zone].bounds.min.x, borders[zone].bounds.max.x),
+            Random.Range(borders[zone].bounds.min.y, borders[zone].bounds.max.y));
+
+         Instantiate(cuteBox[randomCuBindex], pos, Quaternion.identity, cuteGroup);
+         lastCute = Time.time;
+
+         }
     }
 }
